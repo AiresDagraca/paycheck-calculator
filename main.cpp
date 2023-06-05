31c75e1adea5c1007c81caf8f051e969920c8fba
Aires Dagraca <20612015+AiresDagraca@users.noreply.github.com>
2023-06-04 20:40:47 -0500

    test run

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+int hours;
+int rate;
+int otHours;
+
+int otPay;
+int regPay;
+
 int main() {
-    std::cout << "Hello, World! " << std::endl;
+
+    cout << "How many hours did you work this week.\n";
+    cin >> hours;
+    cout << "What is your hourly rate.\n";
+    cin >> rate;
+
+    cout << "You made " << hours * rate << " dollars this week\n";
+
+    cout << "Awesome.\n";
+    cout << "How many overtime hours did your work.\n";
+    cin >> otHours;
+
+   cout << "Your overtime pay is " <<  rate  << "and your reg pay is " << hours * rate;
+
+
+
     return 0;
 }
