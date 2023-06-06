4506f2a8c1110b741915522655221b25053f4327
Aires Dagraca <20612015+AiresDagraca@users.noreply.github.com>
2023-06-05 19:35:36 -0500

    rework

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 
 using namespace std;
-int hours;
-int rate;
-int otHours;
-
-int otPay;
-int regPay;
 
 int main() {
 
+    int hours;
+    int rate;
+    int otHours;
+
+    double x = 1.5;
+
+   int otPay = rate * x;
+    int regPay;
+
+
     cout << "How many hours did you work this week.\n";
     cin >> hours;
-    cout << "What is your hourly rate.\n";
+    cout << "What is your hourly pay rate.\n";
     cin >> rate;
 
     cout << "You made " << hours * rate << " dollars this week\n";
@@ -21,7 +25,7 @@ int main() {
     cout << "How many overtime hours did your work.\n";
     cin >> otHours;
 
-   cout << "Your overtime pay is " <<  rate  << "and your reg pay is " << hours * rate;
+   cout << "Your overtime pay is " <<  otHours * otPay  << " and your reg pay is " << hours * rate;
 
 
 
